7e5378bc3d005df1ba80eca403f8a0379fef3695
redamokhtari34-star <redamokhtari34@gmail.com>
2026-08-07 11:46:20 +0200

    Delete api/api directory

--- api/api/route.js
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function POST(req) {
-  // On répond 200 pour dire à Stripe que le serveur est bien là
-  return NextResponse.json({ received: true }, { status: 200 });
-}
